69537f02a361f9ded857b8db238232db46f227f8
pcharli <pcharli@test.be>
2026-08-24 11:25:28 +0200

    add install btn

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-alert('test')
+// Install
+const installBtn = document.querySelector('#install')
+let defferedPrompt = null
+
+window.addEventListener('beforeinstallprompt', event => {
+    event.preventDefault()
+    defferedPrompt = event
+    installBtn.classList.remove('hidden')
+    installBtn.addEventListener('click', installApp)
+})
+
+const installApp = () => {
+    defferedPrompt.prompt()
+
+
+    //wait user choice
+    defferedPrompt.userChoice.then(choiceResult => {
+        console.log(choiceResult)
+        if(choiceResult.outcome === 'accepted') {
+                installBtn.classList.add('hidden')
+        }
+        else {
+            console.log('PWA rejected')
+        }
+        defferedPrompt = null
+    })
+}
